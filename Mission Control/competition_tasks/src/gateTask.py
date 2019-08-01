444c14368ccd37c2581c60ce22f1c880774b9d92
EdCarrasco <carrasco.edw@gmail.com>
2019-08-01 14:51:50 -0700

    cleaned up gateTask

--- Mission Control/competition_tasks/src/gateTask.py
+++ Mission Control/competition_tasks/src/gateTask.py
@@ -43,13 +43,6 @@ from std_msgs.msg import Bool, Float64, Int16
 
 import seadragon_states as state
 
-DEPTH_START = 18 #depth is in inches
-YAW_GATE = 1.57 # will change acording to direction of gate
-turn = 0.26 # degrees to turn after track state
-buoy_depth = 36 # depth (in inches) for the buoys
-gate_timer = 10000 #time to pass gate
-BOUY_TASK_DEPTH = 5*12 # in inches
-
 def main():
 	# Initialize node with desired node name - ideally task name
 	rospy.init_node('gateTask')
@@ -67,34 +60,43 @@ def main():
 		'area':'/gate_area'
 	}
 
+	GATE_DEPTH = 18
+	GATE_YAW_1 = 1.57
+	GATE_YAW_OFFSET = -0.017 * 5
+	GATE_YAW_2 = GATE_YAW_1 + GATE_YAW_OFFSET
+	GATE_CONVERGE_TICKS = 5000
+	GATE_FORWARD_TICKS = 10000
+	BUOY_DEPTH = 5 * 12
+	BUOY_YAW = 0.017 * 45
+
 	# Open SMACH container
 	with sm:
 		smach.StateMachine.add('IDLE', state.WaitForTopic('/gate_enable'), 
 			transitions={'done':'DEPTH_PID_ENABLE', 'notdone':'IDLE'})
 		smach.StateMachine.add('DEPTH_PID_ENABLE', state.PublishTopic('/depth_control/pid_enable', True), 
 			transitions={'done':'DIVE_GATE_DEPTH'})
-		smach.StateMachine.add('DIVE_GATE_DEPTH', state.ChangeDepthToTarget(18), 
+		smach.StateMachine.add('DIVE_GATE_DEPTH', state.ChangeDepthToTarget(GATE_DEPTH), 
 			transitions={'done':'YAW_PID_ENABLE', 'notdone':'DIVE_GATE_DEPTH', 'reset':'RESET'})
 		smach.StateMachine.add('YAW_PID_ENABLE', state.PublishTopic('/yaw_control/pid_enable', True), 
 			transitions={'done':'ROTATE_TO_GATE'})
-		smach.StateMachine.add('ROTATE_TO_GATE', state.RotateYawToAbsoluteTarget(1.57), 
+		smach.StateMachine.add('ROTATE_TO_GATE', state.RotateYawToAbsoluteTarget(GATE_YAW_1), 
 			transitions={'done':'TRACK_GATE', 'notdone':'ROTATE_TO_GATE', 'reset':'RESET'})	
 		smach.StateMachine.add('TRACK_GATE', state.TrackObject(gate_topic), 
 			transitions ={'done':'SET_YAW_GATE_OFFSET', 'notdone':'TRACK_GATE', 'reset':'RESET'})
 
 
-		smach.StateMachine.add('SET_YAW_GATE_OFFSET', state.PublishTopicRelative('/yaw_control/state', '/yaw_control/setpoint', Float64, -0.017*5),
+		smach.StateMachine.add('SET_YAW_GATE_OFFSET', state.PublishTopicRelative('/yaw_control/state', '/yaw_control/setpoint', Float64, GATE_YAW_OFFSET),
 			transitions = {'done':'ROTATE_GATE_LEFT', 'notdone':'SET_YAW_GATE_OFFSET', 'reset':'RESET'})
 
-		smach.StateMachine.add('ROTATE_GATE_LEFT', state.WaitForConvergence('/yaw_control/state', Float64, -0.017*5, 0.017, 5000), 
+		smach.StateMachine.add('ROTATE_GATE_LEFT', state.WaitForConvergence('/yaw_control/state', Float64, GATE_YAW_2, 0.017, GATE_CONVERGE_TICKS), 
 			transitions ={'done':'MOVE_FORWARD_GATE', 'notdone':'ROTATE_GATE_LEFT', 'reset':'RESET'})
 		
 
-		smach.StateMachine.add('MOVE_FORWARD_GATE', state.MoveForwardTimed(10000, True), 
+		smach.StateMachine.add('MOVE_FORWARD_GATE', state.MoveForwardTimed(GATE_FORWARD_TICKS, True), 
 			transitions ={'done':'BUOY_DEPTH','notdone':'MOVE_FORWARD_GATE', 'reset':'RESET'})
-		smach.StateMachine.add('BUOY_DEPTH', state.ChangeDepthToTarget(5*12), 
+		smach.StateMachine.add('BUOY_DEPTH', state.ChangeDepthToTarget(BUOY_DEPTH), 
 			transitions ={'done':'ROTATE_TO_BUOY','notdone':'BUOY_DEPTH', 'reset':'RESET'})
-		smach.StateMachine.add('ROTATE_TO_BUOY', state.RotateYawToAbsoluteTarget(0.017 * 45), 
+		smach.StateMachine.add('ROTATE_TO_BUOY', state.RotateYawToAbsoluteTarget(BUOY_YAW), 
 			transitions ={'done':'COMPLETED', 'notdone':'ROTATE_TO_BUOY', 'reset':'RESET'})
 		smach.StateMachine.add('COMPLETED', state.PublishTopic('/task_complete', True), 
 			transitions ={'done':'IDLE'})
